63eee202d50342b11bd86b5673df03880ab1d73e
Daniel Vince <vince.daniel.dev@gmail.com>
2019-11-07 18:21:46 +0100

    Rework LED tests to gather more accurate data.

--- src/Python/04_led_matrix.py
+++ src/Python/04_led_matrix.py
@@ -7,36 +7,46 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-B = (102, 51, 0)
-b = (0, 0, 255)
-S = (205,133,63)
 W = (255, 255, 255)
-
-pixels = [
-    B, B, B, B, B, B, B, B,
-    B, B, B, B, B, B, B, B,
-    B, S, S, S, S, S, S, B,
-    S, S, S, S, S, S, S, S,
-    S, W, b, S, S, b, W, S,
-    S, S, S, B, B, S, S, S,
-    S, S, B, S, S, B, S, S,
-    S, S, B, B, B, B, S, S
-]
+B = (0, 0, 0)
 
 for _ in range(10):
+
+    # Show string message
     sense.clear()
     send_message(Command.START, 'LED_show_message')
-    sense.show_message('TEST')
+    message = 'TEST'
+    for i in range(len(message)):
+        sense.show_letter(message[i])
+        time.sleep(1)
     send_message(Command.STOP, 'LED_show_message')
 
+    # Show an 'A' letter
     sense.clear()
     send_message(Command.START, 'LED_show_letter')
     sense.show_letter('A')
-    time.sleep(5)
+    time.sleep(3)
     send_message(Command.STOP, 'LED_show_letter')
 
+    # Light up pixels for more accurate measurement.
     sense.clear()
-    send_message(Command.START, 'LED_set_pixels')
-    sense.set_pixels(pixels)
-    time.sleep(5)
-    send_message(Command.STOP, 'LED_set_pixels')
+    pixels = [
+        B, B, B, B, B, B, B, B,
+        B, B, B, B, B, B, B, B,
+        B, B, B, B, B, B, B, B,
+        B, B, B, B, B, B, B, B,
+        B, B, B, B, B, B, B, B,
+        B, B, B, B, B, B, B, B,
+        B, B, B, B, B, B, B, B,
+        B, B, B, B, B, B, B, B
+    ]
+    for constraint in range(9):
+        for i in range(constraint):
+            for j in range(constraint):
+                pixels[i * 8 + j] = W
+
+        measurement_message = 'LED_set_pixels_{}'.format(constraint)
+        send_message(Command.START, measurement_message)
+        sense.set_pixels(pixels)
+        time.sleep(3)
+        send_message(Command.STOP, measurement_message)
